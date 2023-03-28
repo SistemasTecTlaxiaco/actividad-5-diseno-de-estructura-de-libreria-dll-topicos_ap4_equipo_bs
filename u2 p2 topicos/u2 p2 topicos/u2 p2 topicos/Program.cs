796fd0f2d9c318ff17d040e8877c6f318a2a8d28
Eduardo Tomas Feria Ortiz <feriaortizeduardotomas@gmail.com>
2023-03-28 09:04:02 -0600

    Banco Consola

---
+++ u2 p2 topicos/u2 p2 topicos/u2 p2 topicos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLi;
+namespace u2_p2_topicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu usuario" );
+            String user = Console.ReadLine();
+            Console.WriteLine("Ingresa tu contraseña");
+            String pas = Console.ReadLine();
+            if (Class1.use(user).Equals(""))
+            {
+                if (Class1.pasw(pas).Equals(""))
+                {
+                    Console.WriteLine("Ingresando...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error \n");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
